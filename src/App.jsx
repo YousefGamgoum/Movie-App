1731bf2d0806299626f352c4cadef0fcf46b8e9e
YousefGamgoum <yousefsalah959@gmail.com>
2025-04-11 22:42:19 +0200

    Initialization commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
